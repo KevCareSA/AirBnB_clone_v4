beeacb3e3522c391299f1e9d921fed5afdb0ee22
BradleyGilden <bradleygilden@gmail.com>
2023-11-03 21:34:59 +0200

    implementing front end data filling

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -9,11 +9,11 @@ $(document).ready(() => {
     $('.amenities h4').text(Object.keys(amenityId).sort().join(', '));
   });
 
-  $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+  $.getJSON('http://127.0.0.1:5001/api/v1/status/', (data) => {
     if (data.status === 'OK') {
-      $('div#status_api').addClass('available');
+      $('div#api_status').addClass('available');
     } else {
-      $('div#status_api').removeClass('available');
+      $('div#api_status').removeClass('available');
     }
   });
 });
