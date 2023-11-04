6205d38e60ae9ed243738c6ade70a44e18c8c4a7
KevCareSA <kev.mkbn2@gmail.com>
2023-11-04 13:41:59 +0200

    3-hbnb

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,3 +1,11 @@
+/**
+ * A JavaScript script (static/scripts/3-hbnb.js):
+ * Based on 2-hbnb.js
+ *
+ * Author: Bradley Dillion Gilden And Lebohang KevCare Mokobane
+ * Date: 04-11-2023
+ */
+
 $(document).ready(() => {
   const amenityId = {};
   const HOST = 'http://127.0.0.1:5001';
@@ -19,37 +27,37 @@ $(document).ready(() => {
   });
 
   $.post({
-		url: `${HOST}/api/v1/places_search`,
-		data: JSON.stringify({}),
-		headers: {
-			"Content-Type": "application/json",
-		},
-		success: (data) => {
-			data.forEach((place) =>
-				$("section.places").append(
-					`<article>
-			<div class="title_box">
-			<h2>${place.name}</h2>
-			<div class="price_by_night">$${place.price_by_night}</div>
-			</div>
-			<div class="information">
-			<div class="max_guest">${place.max_guest} Guest${
-						place.max_guest !== 1 ? "s" : ""
-					}</div>
-			<div class="number_rooms">${place.number_rooms} Bedroom${
-						place.number_rooms !== 1 ? "s" : ""
-					}</div>
-			<div class="number_bathrooms">${place.number_bathrooms} Bathroom${
-						place.number_bathrooms !== 1 ? "s" : ""
-					}</div>
-			</div> 
-			<div class="description">
-			${place.description}
-			</div>
-				</article>`
-				)
-			);
-		},
-		dataType: "json",
-	});
+    url: `${HOST}/api/v1/places_search`,
+    data: JSON.stringify({}),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    success: (data) => {
+      data.forEach((place) =>
+        $('section.places').append(
+                `<article>
+        <div class="title_box">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">$${place.price_by_night}</div>
+        </div>
+        <div class="information">
+        <div class="max_guest">${place.max_guest} Guest${
+                    place.max_guest !== 1 ? 's' : ''
+                }</div>
+        <div class="number_rooms">${place.number_rooms} Bedroom${
+                    place.number_rooms !== 1 ? 's' : ''
+                }</div>
+        <div class="number_bathrooms">${place.number_bathrooms} Bathroom${
+                    place.number_bathrooms !== 1 ? 's' : ''
+                }</div>
+        </div> 
+        <div class="description">
+        ${place.description}
+        </div>
+            </article>`
+        )
+      );
+    },
+    dataType: 'json'
+  });
 });
